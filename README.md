6ec51399a11192042b84f0a4fc597fd955103a86
Chris Yanez <cy2019@hotmail.com>
2024-06-29 15:51:03 -0400

    random id made, made modal to take in inputs, got date working

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 The point of this assignment is to see if I can use modals to create a scheduling page
 In this assignment I will be able to create a page that has drag and drop features
 The users will be able to create a task, a due date, and a basic description of the assignment and place it in categories depending on the urgency of the assignment.
+These tasks will be kept in localStorage and be able to be arranged when moved around.
 
-The tasks should be able to remain when the page is refreshed
+The tasks should be able to remain when the page is refreshed.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,47 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
-
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
-
+function getTaskData() {
+    let taskList = JSON.parse(localStorage.getItem("tasks"));
+    let nextId = JSON.parse(localStorage.getItem("nextId"));
 }
 
+
+function init() {
+    $('#deadline').datepicker();
+}
+init();
+// Todo: create a function to generate a unique task id
+
+function generateTaskId() {
+    const min = Math.pow(10, 14); // Minimum 15-digit number
+    const max = Math.pow(10, 15) - 1; // Maximum 15-digit number
+    
+    // Generate a random number between min and max
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return randomNumber;
+
+}
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskID = generateTaskId();
+    const $taskTitle = $('#task-name');
+    const $taskInfo = $('#description-text');
+    const $dueDate = $('#deadline');
 
+    const task = {
+        id: taskID,
+        title: $taskTitle.val(),
+        info: $taskInfo.val(),
+        duedate: $dueDate.val(),
+        done: false
+    };
+    const taskList = getTaskData();
+    taskList.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    $taskTitle.val('');
+    $taskInfo.val('');
+    $dueDate.val('');
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -24,6 +56,7 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+
 
 }
 
